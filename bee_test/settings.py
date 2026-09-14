bf27275e0ec2aec916cc1e9ccdf2d5162b09795d
NurdauletTursynkhan <nurdatursn@gmail.com>
2026-09-14 15:10:36 +0500

    Configure Supabase database

--- bee_test/settings.py
+++ bee_test/settings.py
@@ -47,11 +47,11 @@ TEMPLATES = [
 WSGI_APPLICATION = "bee_test.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
+}pip install dj-database-url
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
